c2e91863f49550a8d0f46437077c74b621a22253
Darrell Abes <dabes1@hotmail.com>
2015-07-20 19:28:33 -0700

    Adding Post Methods

---
+++ WCFWebServiceConsumer1/DataObjects/LoadObjects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFWebServiceConsumer1.DataObjects
+{
+    public class LoadObjects
+    {
+        public int Id { get; set; }
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public string Desc1 { get; set; }
+        public string Desc2 { get; set; }
+    }
+}
